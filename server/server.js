c7469e67e769153ade6620a06a23be32a8f8cf24
Donato Wolfisberg <d.w@hotmail.ch>
2017-02-16 22:22:24 +0100

    added Databas connection

--- server/server.js
+++ server/server.js
@@ -1,60 +1,16 @@
-#!/usr/bin/env node
-var WebSocketServer = require('websocket').server;
-var http = require('http');
+var MongoClient = require('mongodb').MongoClient;
 
-var connections = [];
+var MongoDBConnectionProvider = require('./MongoDBConnectionProvider');
 
-var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
-});
-server.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
-});
+var MqttAdmin = require('./pusch/mqtt/MqttAdmin');
 
-wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
-});
 
-function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
+var admins = [];
+
+var cli = new MongoDBConnectionProvider(onDBConnect);
+
+function onDBConnect() {
+  db = MongoDBConnectionProvider.static.getDatabase();
+
+  admins.push(new MqttAdmin(db));
 }
-
-wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
-    }
-    if (request.requestedProtocols.indexOf('echo-protocol') === -1) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection rejected wrong protocol: ' + request.requestedProtocols);
-      return;
-    }
-
-    var connection = request.accept('echo-protocol', request.origin);
-    console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
-    });
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
-});
